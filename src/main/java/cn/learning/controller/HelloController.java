1b413bd45ed7aaeef733ce91b0f778571f8165c6
minxiaoxian <minxiaoxian@peopelyun.cn>
2019-08-13 11:03:16 +0800

    test

--- src/main/java/cn/learning/controller/HelloController.java
+++ src/main/java/cn/learning/controller/HelloController.java
@@ -3,18 +3,13 @@ package cn.learning.controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 @RestController
 public class HelloController {
 
-	private static final Logger LOG = LoggerFactory.getLogger(HelloController.class);
-	
 	@RequestMapping("/hello/{name}")
 	public String hello(@PathVariable String name) {
 
-		LOG.info("---------------TEST------------");
 		System.out.println("test------------");
 		return "1 springboot hello"+name;
 	}
