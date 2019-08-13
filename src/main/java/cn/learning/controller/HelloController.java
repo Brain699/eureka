30418aa400a8fe56446ee49665478ed2e0c03497
minxiaoxian <minxiaoxian@peopelyun.cn>
2019-08-13 10:39:49 +0800

    test

--- src/main/java/cn/learning/controller/HelloController.java
+++ src/main/java/cn/learning/controller/HelloController.java
@@ -9,6 +9,8 @@ public class HelloController {
 	
 	@RequestMapping("/hello/{name}")
 	public String hello(@PathVariable String name) {
+
+		System.out.println("test------------")
 		return "1 springboot hello"+name;
 	}
 
